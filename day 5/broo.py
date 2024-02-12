1737404d6bc6931e3e34290b14bdc88e6b39e9f9
GiorgiabaZakhidze <giorgizabakhidze2011@gmail.com>
2024-02-12 22:52:09 +0400

    commit

---
+++ day 5/broo.py
@@ -0,0 +1,18 @@
+ #print("hi i am giorgi")
+#print("i love basketball")
+
+#frist_name = "bro"
+
+# print("hello"+ name)
+#age = 21
+#age = age + 1
+#print(age)
+##print(type(age))
+#print("ur age is "+str(age))
+#print("ur age is "+str(age))
+#height = 250.5
+#print("ur height is" + str(height)+"cm")
+#print(type(height))
+human = True
+print("are u a human " + str(human))
+print(type(human))
