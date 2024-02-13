cf8ead08a209d001fc8079520ff06cf0f95f8b6f
GiorgiabaZakhidze <giorgizabakhidze2011@gmail.com>
2024-02-13 11:03:20 +0400

    commit

--- day 5/broo.py
+++ day 5/broo.py
@@ -1,18 +1,68 @@
- #print("hi i am giorgi")
-#print("i love basketball")
+# # print("hi i am giorgi")
+# # print("i love basketball")
 
-#frist_name = "bro"
+# # frist_name = "bro"
 
-# print("hello"+ name)
-#age = 21
-#age = age + 1
-#print(age)
-##print(type(age))
-#print("ur age is "+str(age))
-#print("ur age is "+str(age))
-#height = 250.5
-#print("ur height is" + str(height)+"cm")
-#print(type(height))
-human = True
-print("are u a human " + str(human))
-print(type(human))
+# # print("hello"+ name)
+# # age = 21
+# # age = age + 1
+# # print(age)
+# # print(type(age))
+# # print("ur age is "+str(age))
+# # print("ur age is "+str(age))
+# # height = 250.5
+# # print("ur height is" + str(height)+"cm")
+# # print(type(height))
+# # human = True
+# # print("are u a human " + str(human))
+# # print(type(human))
+# name = input("what is ur name?> ")
+# age = int(input("how old are u?> "))
+# height = float(input("how tall are u?> "))
+
+
+# print(f"hello {name}")
+# print(f"ur age is {age}")
+# print("ur height is " +str(height)+"cm tall")
+# import math
+
+
+# pi = -3.14
+# x = 1
+# y = 2
+# z = 3
+
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi,2))
+# print(math.sqrt(420))
+# # print(max(x,y,z))
+# print(min(x,y,z))
+
+
+# name = "bro code"
+
+# first_name = name[:3]
+# last_name = name[4:]
+# funky_name = name[0:8:3]
+# reversed_name = name[::-1]
+
+# print(reversed_name)
+# website1 = "http://google.com"
+# website2 = "http://wikipedia.com"
+# slice = slice(7,-4)
+
+# print(website1[slice])
+# print(website2[slice])
+# age =  int(input("how old are u> "))
+
+# if age == 100:
+#     print("u are a century old!")
+# elif age <= 18:
+#     print("u are an adult!")
+# elif age < 0:
+#     print("u haven't even been born yet!")
+# elif age < 18:
+#     print("go home kido u are child!")
---
+++ day 5/gio.py
@@ -0,0 +1,23 @@
+# name = input("enter ur name> ") 
+# age = input("what is ur age> ")
+
+# print(f"hello {name}, ur are {age}")
+# #print(f"hello {name}")
+# age = int(input("enter ur age"))
+# if age >= 18:
+#     print("u are allowed to enter the culb")
+# elif age < 18:
+#     print("go home kid (L bozo)" )   
+
+# print("BF CALCULATOR")
+# weigth = int(input("enter ur weight> "))
+# if weigth >= 90:
+#     print("u aint geting any bitches until u lose some fat")
+# elif weigth < 90:
+#     print("boy u can get some bitches with that weihgt")
+# 
+# height = int(input("what is ur height> "))
+# if height >= 190:
+#     print("u can make it")
+# elif height < 190:
+#     print("bro just give up")
