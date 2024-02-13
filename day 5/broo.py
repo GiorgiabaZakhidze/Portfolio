a9724042783ad29aba3c63ae2374e970756895e9
GiorgiabaZakhidze <giorgizabakhidze2011@gmail.com>
2024-02-13 23:10:55 +0400

    commit

--- day 5/broo.py
+++ day 5/broo.py
@@ -1,68 +1,75 @@
-# # print("hi i am giorgi")
-# # print("i love basketball")
+print("hi i am giorgi")
+print("i love basketball")
 
-# # frist_name = "bro"
+frist_name = "bro"
 
-# # print("hello"+ name)
-# # age = 21
-# # age = age + 1
-# # print(age)
-# # print(type(age))
-# # print("ur age is "+str(age))
-# # print("ur age is "+str(age))
-# # height = 250.5
-# # print("ur height is" + str(height)+"cm")
-# # print(type(height))
-# # human = True
-# # print("are u a human " + str(human))
-# # print(type(human))
-# name = input("what is ur name?> ")
-# age = int(input("how old are u?> "))
-# height = float(input("how tall are u?> "))
-
-
-# print(f"hello {name}")
-# print(f"ur age is {age}")
-# print("ur height is " +str(height)+"cm tall")
-# import math
+print("hello"+ name)
+age = 21
+age = age + 1
+print(age)
+print(type(age))
+print("ur age is "+str(age))
+print("ur age is "+str(age))
+height = 250.5
+print("ur height is" + str(height)+"cm")
+print(type(height))
+human = True
+print("are u a human " + str(human))
+print(type(human))
+name = input("what is ur name?> ")
+age = int(input("how old are u?> "))
+height = float(input("how tall are u?> "))
 
 
-# pi = -3.14
-# x = 1
-# y = 2
-# z = 3
-
-# print(round(pi))
-# print(math.ceil(pi))
-# print(math.floor(pi))
-# print(abs(pi))
-# print(pow(pi,2))
-# print(math.sqrt(420))
-# # print(max(x,y,z))
-# print(min(x,y,z))
+print(f"hello {name}")
+print(f"ur age is {age}")
+print("ur height is " +str(height)+"cm tall")
+import math
 
 
-# name = "bro code"
+pi = -3.14
+x = 1
+y = 2
+z = 3
 
-# first_name = name[:3]
-# last_name = name[4:]
-# funky_name = name[0:8:3]
-# reversed_name = name[::-1]
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi,2))
+print(math.sqrt(420))
+print(max(x,y,z))
+print(min(x,y,z))
 
-# print(reversed_name)
-# website1 = "http://google.com"
-# website2 = "http://wikipedia.com"
-# slice = slice(7,-4)
 
-# print(website1[slice])
-# print(website2[slice])
-# age =  int(input("how old are u> "))
+name = "bro code"
 
-# if age == 100:
-#     print("u are a century old!")
-# elif age <= 18:
-#     print("u are an adult!")
-# elif age < 0:
-#     print("u haven't even been born yet!")
-# elif age < 18:
-#     print("go home kido u are child!")
+first_name = name[:3]
+last_name = name[4:]
+funky_name = name[0:8:3]
+reversed_name = name[::-1]
+
+print(reversed_name)
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+slice = slice(7,-4)
+
+print(website1[slice])
+print(website2[slice])
+age =  int(input("how old are u> "))
+
+if age == 100:
+    print("u are a century old!")
+elif age <= 18:
+    print("u are an adult!")
+elif age < 0:
+    print("u haven't even been born yet!")
+elif age < 18:
+    print("go home kido u are child!")
+while 1==1:
+     print("help i am sutck in a loop")
+name = None
+while not name:
+    name = input("enter ur name: ")
+
+print("hello "+name)
